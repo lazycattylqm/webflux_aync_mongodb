4644b090a1f8a5278cd4c3bb5491b7dbe005509d
lazycattylqm <leeqiming@163.com>
2023-06-04 11:33:54 +0800

    first commit

---
+++ src/main/java/com/async/monog/async_mongo/AsyncMongoApplication.java
@@ -0,0 +1,13 @@
+package com.async.monog.async_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/async/monog/async_mongo/AsyncMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.async.monog.async_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
